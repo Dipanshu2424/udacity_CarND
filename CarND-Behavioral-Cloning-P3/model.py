f0ae18b7f9169af9c916ad6f921de7351933a86b
Maria Rigaki <mrigaki@gmail.com>
2017-07-02 18:05:20 +0200

    Model that passes the test

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -2,53 +2,74 @@ import csv
 import cv2
 import numpy as np 
 from keras.models import Sequential 
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Conv2D, MaxPooling2D, Dropout
+import sklearn
+
 
 lines = []
-with open('../data/behavioral/data/driving_log.csv') as csvfile:
+with open('../data/behavioral2/driving_log2.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
 
-images = []
-measurements = []
+car_images = []
+steering_angles = []
+
 for line in lines:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = '../data/behavioral/data/IMG/' + filename
-	image = cv2.imread(current_path)
-	measurement = float(line[3])
-	images.append(image)
-	measurements.append(measurement)
-	image_flipped = np.fliplr(image)
-	measurement_flipped = -measurement
-	images.append(image_flipped)
-	measurements.append(measurement_flipped)
+	steering_center = float(line[3])
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+	# create adjusted steering measurements for the side camera images
+	correction = 0.29 # this is a parameter to tune
+	steering_left = steering_center + correction
+	steering_right = steering_center - correction
+	
+	img_center = cv2.imread(line[0])
+	img_left = cv2.imread(line[1])
+	img_right = cv2.imread(line[2])
+
+	# add images and angles to data set
+	car_images.append(img_center)
+	car_images.append(img_left)
+	car_images.append(img_right)
+	steering_angles.append(steering_center)
+	steering_angles.append(steering_left)
+	steering_angles.append(steering_right)
+
+
+X_train = np.array(car_images)
+y_train = np.array(steering_angles)
+
 input_shape = (160, 320, 3)
-nb_epoch = 3
+nb_epoch = 5
 
+# Define the CNN in keras
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=input_shape))
-#model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((50,30), (0,0)), input_shape=input_shape))
 
-model.add(Conv2D(16, kernel_size=(5, 5),
-                 activation='relu',
-                 input_shape=input_shape))
+# Convolutional layer 1
+model.add(Conv2D(16, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Convolutional layer 2
 model.add(Conv2D(16, (5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+
+# Convolutional layer 1
+model.add(Conv2D(16, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.75))
+
+# Fully connected layer
 model.add(Flatten())
-model.add(Dense(64, activation='relu'))
+model.add(Dense(100))
 model.add(Dropout(0.5))
+
+# Output
 model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=nb_epoch)
-
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=nb_epoch, batch_size=128)
 model.save('model.h5')
-
